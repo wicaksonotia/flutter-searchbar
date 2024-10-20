a784281dc7ea5d91b1daec0dd17b2c0858b73980
Tia Wicaksono <wicaksono.tia@gmail.com>
2024-10-20 16:15:34 +0700

    sliver app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,11 @@
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gap/gap.dart';
-import 'package:searchbar/containers/container_appbar.dart';
+import 'package:searchbar/common/colors.dart';
+import 'package:searchbar/containers/container_search.dart';
+// import 'package:searchbar/containers/container_tab_bar.dart';
 import 'package:searchbar/pages/bloc/product/product_bloc.dart';
 import 'package:searchbar/network_manager/dio_helper.dart';
-import 'package:searchbar/pages/bloc/slider_promo/slider_promo_bloc.dart';
-import 'package:searchbar/widgets/widget_header_seeall.dart';
-import 'package:searchbar/widgets/widget_slider.dart';
-import 'package:searchbar/widgets/widget_special_for_you.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,16 +14,52 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  List<String> tabBarItems = [
+    "Home",
+    "Explore",
+    "Search",
+  ];
+
+  TabController? tabController;
+  int index = 0;
+  String image =
+      "https://images.pexels.com/photos/1283219/pexels-photo-1283219.jpeg?auto=compress&cs=tinysrgb&w=600";
+  List<String> dataImages = [
+    "https://images.pexels.com/photos/1283219/pexels-photo-1283219.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://umkm.averroes.or.id/assets/assets_artikel/d0bd99163261318d5675e320af42c52d.jpeg",
+    "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600",
+  ];
+
+  void _tabListener() {
+    setState(() {
+      index = tabController!.index;
+      image = dataImages[index];
+    });
+  }
+
+  @override
+  void initState() {
+    tabController = TabController(
+      // initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+    tabController!.addListener(_tabListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController!.removeListener(_tabListener);
+    tabController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<SliderPromoBloc>(
-          create: (context) => SliderPromoBloc(
-            RemoteDataSource(),
-          ),
-        ),
         BlocProvider<ProductBloc>(
           create: (context) => ProductBloc(
             RemoteDataSource(),
@@ -34,19 +68,70 @@ class _HomeScreenState extends State<HomeScreen> {
       ],
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.only(top: 40, right: 10, left: 10),
-          child: Column(
-            children: [
-              AppBarContainer(),
-              Gap(10),
-              sliderWidget(),
-              Gap(10),
-              HeaderSeeAllWidget(
-                header: "Special for you",
+          padding: const EdgeInsets.only(top: 10),
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: SearchBarContainer(),
+                backgroundColor: MyColors.main,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.parallax,
+                  background: Image.network(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(40),
+                  child: Container(
+                    color: Colors.white,
+                    alignment: Alignment.centerLeft,
+                    child: TabBar(
+                      controller: tabController,
+                      labelColor: Colors.white,
+                      unselectedLabelColor: Colors.grey,
+                      // indicator:
+                      //     CircleTabIndicators(color: MyColors.main, radius: 5),
+                      indicator: BubbleTabIndicator(
+                        indicatorColor: MyColors.main,
+                        indicatorRadius: 5,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 20.0),
+                      ),
+                      isScrollable: true,
+                      labelPadding: EdgeInsets.only(left: 20, right: 20),
+                      tabAlignment: TabAlignment.start,
+                      dividerColor: Colors.transparent,
+                      tabs:
+                          tabBarItems.map((label) => Tab(text: label)).toList(),
+                    ),
+                  ),
+                ),
+                // backgroundColor: Colors.white,
+                pinned: true,
+                expandedHeight: 200,
               ),
-              Gap(10),
-              productWidget()
-              // TabBarContainer(),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 800,
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      // productWidget(),
+                      Text("Home"),
+                      Text("Inspirations"),
+                      Text("Emotions"),
+                    ],
+                  ),
+                ),
+              )
+              // SliverList(
+              //   delegate: SliverChildBuilderDelegate(
+              //       (context, index) => ListTile(
+              //             title: Text('item $index'),
+              //           ),
+              //       childCount: 100),
+              // )
             ],
           ),
         ),
